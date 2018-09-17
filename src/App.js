105aa6985c901b98bc7e51aed8baf0d20509dbd7
Deepak Purushotham <dpurushotham136@gmail.com>
2018-09-17 11:44:58 -0500

    Added Router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class App extends Component {
     state = {}
     render() {
-        return (App);
+        return (
+            <div className="fdsafd">App</div>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './App';
+import AppRouter from './AppRouter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
